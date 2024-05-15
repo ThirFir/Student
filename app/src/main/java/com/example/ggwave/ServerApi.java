0a6860f6a2170e4664483eef2d08ab4dd9f57624
DoHyeok Kim <hyeokdokim@gmail.com>
2024-05-15 13:21:34 +0900

    이전 작업 커밋

--- app/src/main/java/com/example/ggwave/ServerApi.java
+++ app/src/main/java/com/example/ggwave/ServerApi.java
@@ -4,10 +4,13 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ServerApi {
     @POST("")
     Call<AttendanceDTO> postDecodedKey(@Body KeyReq req);
 
 
+    @GET("login")
+    Call<Boolean> getIsLoginSuccess(@Query("id") String id);
 }
